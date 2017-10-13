1ae7aa71b75c97357d1b157e0422abe88f3b18a3
JaccoJongenotter <22053@ma-web.nl>
2017-10-13 20:59:54 +0200

    Full game

---
+++ GameScripts/Library/Collab/Base/Assets/Scripts/PlayerDeathCol.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathCol : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            PlayerManager.instance.BirdDied();
+        }
+    }
+}
